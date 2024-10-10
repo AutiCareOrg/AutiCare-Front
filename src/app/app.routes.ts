e79fa53b72a1ee59b05b5a5db3fb2a6936faa624
jvJUCA <jucafranzoni1@gmail.com>
2024-10-10 20:08:18 -0300

    feat: improved router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LandingComponent } from './landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'landing', component: LandingComponent },
+    { path: '', redirectTo: '/landing', pathMatch: 'full' }
+];
