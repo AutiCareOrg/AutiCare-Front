77071a94e8148737eb415cb22294916d3abd0dea
JoaoAngelotti17 <joaopedroangelotti@gmail.com>
2024-10-20 16:21:00 -0300

    componente about-us criado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'landing', component: LandingComponent },
+    { path: 'about-us', component: AboutUsComponent },
     { path: 'login', component: LoginComponent },
     { path: '', redirectTo: '/landing', pathMatch: 'full' }
 ];
