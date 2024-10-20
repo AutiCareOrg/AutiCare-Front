09f6b429f79537651e20c756516f819a092d412b
Giovanni Nogueira <giovanni-nog@hotmail.com>
2024-10-20 18:15:03 -0300

    Registration screen and functionality screen created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LandingComponent } from './landing/landing.component';
+import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { FuncionalidadesComponent } from './funcionalidades/funcionalidades.component';
 
 export const routes: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'landing', component: LandingComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'funcionalidades', component: FuncionalidadesComponent },
     { path: '', redirectTo: '/landing', pathMatch: 'full' }
 ];
